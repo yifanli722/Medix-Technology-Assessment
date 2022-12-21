2409eba5186cdde252b6401d6ab6a4320b725694
yifanli722 <yifanli722@gmail.com>
2022-12-21 08:07:33 -0700

    solution for #6

--- SQL_problems/sql_solutions/6.sql
+++ SQL_problems/sql_solutions/6.sql
@@ -2,10 +2,22 @@
 -- that have a matching billing address (address_type = 'BILL').
 -- If a matching billing address does not exist, display 'NONE' in the address_type column.
 
--- NOT CORRECT
-SELECT p.first_name, p.last_name, a.street_line_1, a.city
+SELECT p.first_name,
+	p.last_name,
+    (CASE WHEN a.address_type IS NULL THEN 'NONE' ELSE a.address_type END) AS address_type,
+   	a.street_line_1,
+	a.city,
+	a.state,
+	a.zip_code
 FROM person p
-JOIN address a ON p.person_id = a.person_id
-JOIN address a2 ON p.person_id = a2.person_id AND a.address_id <> a2.address_id
-GROUP BY p.person_id, a.street_line_1
-HAVING COUNT(*) >= 2;
+LEFT JOIN address a ON p.person_id = a.person_id AND a.address_type = 'BILL'
+WHERE EXISTS (SELECT person_id, address_type, street_line_1, city, state, zip_code
+			  FROM address
+			  WHERE person_id = p.person_id
+				  AND address_type = 'HOME'
+				  AND street_line_1 = a.street_line_1
+				  AND city = a.city
+				  AND state = a.state
+				  AND zip_code = a.zip_code
+			 )
+OR a.address_type IS NULL;
