96d2f57da941e26bf435822b75bccd6f0946d1bf
yifanli722 <yifanli722@gmail.com>
2022-12-21 08:29:47 -0700

    results for 9

--- SQL_problems/sql_solutions/9.sql
+++ SQL_problems/sql_solutions/9.sql
@@ -1,3 +1,6 @@
 -- Write a query to update the person.occupation column to
 -- ‘X’ for all rows that have a ‘BILL’ address in the address table.
-SKIP
+UPDATE person
+SET occupation = 'X'
+WHERE EXISTS(SELECT 1 FROM address WHERE person.person_id = address.person_id AND address.address_type = 'BILL')
+
